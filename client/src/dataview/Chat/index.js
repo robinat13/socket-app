9b79cbf9c2b786a4b0e4dbb9a1b7a6a2f8c39cc5
Robin Chauhan <robinthehood13@gmail.com>
2021-03-06 14:17:53 +0530

    Fixed cors issue

--- client/src/dataview/Chat/index.js
+++ client/src/dataview/Chat/index.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
 
-const Chat = () => {
+let socket;
+
+const Chat = ({ location }) => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+    socket = io("http://localhost:5000");
+    setName(name);
+    setRoom(room);
+    console.log(socket);
+  }, []);
+
   return <h1>Chat</h1>;
 };
 
--- client/src/dataview/Home/index.js
+++ client/src/dataview/Home/index.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import s from "./style.module.css";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <h1>Home</h1>;
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
+  return (
+    <div className={s.joinOuterContainer}>
+      <div className={s.joinInnerContainer}>
+        <h1 className={s.heading}>Join</h1>
+        <div>
+          <input
+            placeholder="Name"
+            className={s.joinInput}
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+          ></input>
+        </div>
+        <div>
+          <input
+            placeholder="Room"
+            className={`${s.joinInput} ${s["mt-20"]}`}
+            type="text"
+            onChange={(e) => setRoom(e.target.value)}
+          ></input>
+        </div>
+        <Link
+          onClick={(e) => (!name || !room ? e.preventDefault() : null)}
+          to={`/chat?name=${name}&room=${room}`}
+        >
+          <button className={`${s.button} ${s["mt-20"]}`} type="submit">
+            Sign In
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
